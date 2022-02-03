66c493f7594b2c395746c4caa41acc4adc9d5c73
Ahmed Negm <ahmdn4418@gmail.com>
2022-02-03 07:12:04 +0200

    test

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  let navigation = document.querySelector(".side-bar");
+  let toggle = document.querySelector(".toggle");
+  navigation.classList.toggle("opened");
+  toggle.classList.toggle("opened");
+}
+function myFunction() {
+  var element = document.getElementById("link");
+  element.classList.toggle("d-none");
+}
